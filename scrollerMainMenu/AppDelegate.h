8a7132b62e9971ec4ba81673d3b9966e8164b4f5
A.J <aj@openmetrics.com>
2017-11-12 07:40:39 -0800

    Initial Commit

---
+++ scrollerMainMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  scrollerMainMenu
+//
+//  Created by aj on 11/12/17.
+//  Copyright © 2017 aj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ scrollerMainMenu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  scrollerMainMenu
+//
+//  Created by aj on 11/12/17.
+//  Copyright © 2017 aj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
